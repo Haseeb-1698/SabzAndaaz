473fe36df73ae5139ff09881ec99ec9a63f7b22f
Muhammad Haseeb <i221698@nu.edu.pk>
2025-04-27 00:21:10 +0500

    Update page.tsx

--- sabaz-andaaz-web-next/src/app/page.tsx
+++ sabaz-andaaz-web-next/src/app/page.tsx
@@ -1,12 +1,14 @@
 import AppComponent from '../components/AppComponent';
 import '../components/App.css';
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
     <div>
       <AppComponent />
       <SpeedInsights />
+      <Analytics />
     </div>
   );
 }
